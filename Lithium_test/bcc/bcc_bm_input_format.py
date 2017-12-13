1ad79f88c0c934522bc08d402b53ca9cc11cc212
Max Tyler <mtyler88@gmail.com>
2017-12-13 17:55:56 +0000

    Add new fcc/bcc values with dft energy included

---
+++ Lithium_test/bcc/bcc_bm_input_format.py
@@ -0,0 +1,34 @@
+import sys
+
+filenames = {}
+for argument in sys.argv[1:]:
+    print(argument)
+    v=(argument.split(".")[3:5])
+    fname=argument.split("/")[2]
+    filenames[fname]=float(v[0]+"."+v[1])
+
+temps = []
+with open("extracted/"+next(iter(filenames.keys()))) as f:
+    for line in f:
+        temps.append(float(line.split()[0]))
+
+tempdict={}
+for temp in temps:
+    tempdict[temp]=[]
+
+for filename in sorted(filenames, key=filenames.__getitem__):
+    with open("extracted/bcc.fqha."+str(filenames[filename])) as f:
+        with open("extracted/bcc-li.scf.out."+str(filenames[filename])) as dft:
+            for line in dft:
+                if "!" in line:
+                    dft_fe=float(line.split()[4])
+                    print(dft_fe)
+            for line in f:
+                temp=float(line.split()[0])
+                fe=float(line.split()[1])
+                tempdict[temp].append( (filenames[filename], fe+dft_fe) )
+
+for temp in tempdict:
+    with open("bm_fits/"+"li.bcc."+str(temp), "w") as f:
+        for (vol, fe) in tempdict[temp]:
+            f.write(str(vol)+"\t"+str(fe)+"\n")
