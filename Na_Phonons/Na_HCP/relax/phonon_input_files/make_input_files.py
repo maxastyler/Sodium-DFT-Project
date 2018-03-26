d81d564f8fbb9252510b82a591564f8513b816b8
Max Tyler <mtyler88@gmail.com>
2018-03-26 18:09:32 +0100

    Export ph files too

--- Na_Phonons/Na_HCP/relax/phonon_input_files/make_input_files.py
+++ Na_Phonons/Na_HCP/relax/phonon_input_files/make_input_files.py
@@ -41,9 +41,25 @@ ATOMIC_POSITIONS (crystal)\n\
  Na  0.0 0.0 0.0\n\
  Na  0.6666666 0.3333333 0.5\n\
 K_POINTS automatic\n\
- 24 24 12 0 0 1\n\
-EOF"
+ 24 24 12 0 0 1"
+
+p_s = "Na HCP Phonon Calculation\n\
+&inputph\n\
+  prefix='na_hcp_{a}',\n\
+  outdir='./na_hcp_{a}/',\n\
+  fildyn='na_hcp_{a}.dyn',\n\
+\n\
+! Convergence settings:\n\
+  tr2_ph=1e-15,\n\
+\n\
+! Set up regular q-grid for DFPT:\n\
+  ldisp=.true.,\n\
+  nq1=4, nq2=4, nq3=4\n\
+ /"
 
 for (a, ca) in vals:
-    with open("na_hcp_{:.3f}_in".format(a), "w") as f:
+    with open("na_hcp_scf_{}_in".format(a), "w") as f:
     	f.write(f_s.format(a=a, ca = ca))
+
+    with open("na_hcp_ph_{}_in".format(a), "w") as f:
+    	f.write(p_s.format(a=a, ca = ca))
