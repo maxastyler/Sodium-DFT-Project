1f73fee8fb33090b18a5b77dd6171b369872db1c
Max Tyler <mtyler88@gmail.com>
2018-03-27 10:15:15 +0100

    Add process hcp sn phonons bash script to make dos

---
+++ Na_Phonons/Na_HCP/phonons/others/process_phonons.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+for v in 6.976382950545051 6.998233353524401 6.993850699385001 6.98348186215975 6.99410908717325; do
+	mpirun -np 4 q2r.x > na_hcp_q2r_$v.out << EOF
+&input
+	fildyn = 'na_hcp_$v.dyn',
+	flfrc = 'na_hcp_$v.fc',
+	zasr = 'simple',
+EOF
+
+	mpirun -np 4 matdyn.x > na_hcp_dos_$v.out << EOF
+&input
+        asr = 'simple',
+        flfrc = 'na_hcp_$v.fc',
+        dos = .true.,
+        nk1 = 30, nk2 = 30, nk3 = 30,
+        fldos = 'na_hcp_$v.dos',
+/
+EOF
+done
