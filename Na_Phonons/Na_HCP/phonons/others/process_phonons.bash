9a2bbc75af3d5f0cdd41905a73aa1da46d8a441d
Max Tyler <s1333534@cplab070.ph.ed.ac.uk>
2018-03-28 11:42:09 +0100

    Add new density of states

--- Na_Phonons/Na_HCP/phonons/others/process_phonons.bash
+++ Na_Phonons/Na_HCP/phonons/others/process_phonons.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 #for v in 6.976382950545051 6.998233353524401 6.993850699385001 6.98348186215975 6.99410908717325; do
 for v in 6.94 6.96 7.008 7.018 7.03 7.04; do
+	echo "$v"
 	mpirun -np 4 q2r.x > na_hcp_q2r_$v.out << EOF
 &input
 	fildyn = 'na_hcp_$v.dyn',
